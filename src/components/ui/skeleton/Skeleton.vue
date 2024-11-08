7f4d4e107e818e472ce2d65da005af54d8bb89a9
Arief-Af <sekolah.arief@gmail.com>
2024-11-09 00:31:55 +0700

    feat(google_meet): add skeleton

---
+++ src/components/ui/skeleton/Skeleton.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { cn } from '@/components/lib/utils';
+
+const props = defineProps({
+  class: { type: null, required: false },
+});
+</script>
+
+<template>
+  <div :class="cn('animate-pulse rounded-md bg-neutral-100 dark:bg-neutral-800', props.class)" />
+</template>
--- src/pages/google_meet/index.vue
+++ src/pages/google_meet/index.vue
@@ -2,16 +2,31 @@
   <DashboardLayout>
     <header class="text-2xl font-bold text-primary">Google Meet</header>
     <main
+      v-if="loading"
+      class="bg-primary text-white px-10 xl:px-40 rounded-3xl h-[431px] mt-5 flex items-center animate-pulse"
+    >
+      <section>
+        <span class="text-[26px] h-6 bg-gray-200 rounded w-1/4"></span> <br />
+        <span
+          class="font-bold text-[26px] h-6 bg-gray-200 rounded w-1/2"
+        ></span>
+        <br />
+        <span class="text-[20px] h-6 bg-gray-200 rounded w-1/3"></span>
+      </section>
+    </main>
+    <main
+      v-else
       class="bg-primary text-white px-10 xl:px-40 rounded-3xl h-[431px] mt-5 flex items-center"
     >
       <section>
         <span class="text-[26px]">{{ data?.date }} <br /></span>
         <span class="font-bold text-[26px]">{{ data?.title }}</span> <br />
-        <span class="text-[20px]">Bersama kak {{  data?.instructor }} </span>
+        <span class="text-[20px]">Bersama kak {{ data?.instructor }} </span>
       </section>
     </main>
     <div class="text-center mt-5">
       <Button
+        v-if="!loading"
         @click="openNewTab(data?.url)"
         class="px-16 dark:bg-[#449072] dark:text-white dark:hover:bg-primary dark:hover:text-white bg-[#449072] text-white hover:bg-primary hover:text-white"
         >Join</Button
@@ -21,17 +36,19 @@
 </template>
 
 <script setup>
+import { Skeleton } from "@/components/ui/skeleton";
 import DashboardLayout from "@/layouts/dashboard/index.vue";
 import { Button } from "@/components/ui/button";
 import { useMeetStore } from "@/stores/meets";
 import { onMounted, ref } from "vue";
 const meetsStore = useMeetStore();
 const data = ref([]);
+let loading = ref(true);
 const fetchData = async () => {
   try {
     const resp = await meetsStore.getData();
     data.value = resp.data.data;
-    console.log(data.value);
+    loading.value = false;
   } catch (error) {
     console.log(error);
   }
@@ -39,7 +56,7 @@ const fetchData = async () => {
 
 const openNewTab = (url) => {
   window.open(url, "_blank");
-}
+};
 onMounted(async () => {
   await fetchData();
 });
