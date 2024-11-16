45423c3b3f1da9cca8afc53747d581c6ce64adf6
Arief-Af <sekolah.arief@gmail.com>
2024-11-16 18:04:38 +0700

    fix(gemni): api

--- src/components/chatbot/index.vue
+++ src/components/chatbot/index.vue
@@ -9,7 +9,11 @@
   >
     <div
       v-if="active"
-      :class="fullscreen ? 'w-[90%] h-[90%] md:w-[70%] md:-[70%]' : 'h-[450px] w-[350px]'"
+      :class="
+        fullscreen
+          ? 'w-[90%] h-[90%] md:w-[70%] md:-[70%]'
+          : 'h-[450px] w-[350px]'
+      "
       class="dark:bg-[#262626] dark:border dark:border-primary bg-secondary shadow-lg p-5 rounded-3xl"
     >
       <header class="flex gap-2 pb-4 justify-between items-center">
@@ -55,6 +59,29 @@
             class="bg-primary rounded-lg p-5 w-max max-w-[80%] mb-4 text-sm text-white"
           ></div>
         </div>
+        <div class="justify-start" v-if="loading">
+          <div
+            class="bg-primary rounded-lg p-5 w-max max-w-[80%] mb-4 text-sm text-white"
+          >
+            Tunggu Sebentar &nbsp;
+            <svg
+              aria-hidden="true"
+              class="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-primary"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+          </div>
+        </div>
       </main>
       <footer class="relative w-full items-center mt-5">
         <form @submit.prevent="onSendMessage">
@@ -89,13 +116,16 @@ import FormInputBot from "@/components/form/inputBot.vue";
 const { sendMessage } = useChat();
 onMounted(async () => {});
 let fullscreen = ref(false);
+let loading = ref(false);
 const onSendMessage = async () => {
+  loading.value = true;
+  let messageValue = ref(message.value);
   if (!message.value) return;
-
-  chat.value.push({ isBot: false, message: message.value });
-  const resp = await sendMessage(message.value);
   message.value = "";
+  chat.value.push({ isBot: false, message: messageValue.value });
+  const resp = await sendMessage(messageValue.value);
   chat.value.push({ isBot: true, message: resp });
+  loading.value = false;
 };
 
 let message = ref("");
@@ -122,7 +152,7 @@ function formatText(inputText) {
   // Format italic (untuk *italic text*)
   const italicRegex = /\*(.*?)\*/g;
   inputText = inputText.replace(italicRegex, (match, content) => {
-    return `<em class="italic">\n${content}\n</em>`;  // Menambahkan newline sebelum dan sesudah
+    return `<em class="italic">\n${content}\n</em>`; // Menambahkan newline sebelum dan sesudah
   });
 
   // Format underline (untuk __underline text__)
@@ -135,7 +165,6 @@ function formatText(inputText) {
 
   return inputText;
 }
-
 
 let active = ref(false);
 
